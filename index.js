d3f8b5138a2be2c810e6f0192be8c26fa73612fd
maniyarmohammedsaqlain <85744349+maniyarmohammedsaqlain@users.noreply.github.com>
2022-09-11 16:56:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+const ref=document.getElementById("boody")
+
+let width=window.innerWidth;
+
+
+if (width<=600){
+    // ref.classList.remove("body")
+    ref.classList.add("invisible")
+    alert("Please open Desktop Site and Reload to get access of this page ")
+
+}
